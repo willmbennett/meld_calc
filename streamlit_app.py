a9aabce1851236e16d82f60aa6a41cfe5e5405eb
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-06-30 15:25:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
 import streamlit as st
-PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
-st.beta_set_page_config(**PAGE_CONFIG)
-def main():
-	st.title("Awesome Streamlit for ML")
-	st.subheader("How to run streamlit from colab")
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox('Menu',menu)
-	if choice == 'Home':
-		st.subheader("Streamlit From Colab")	
-if __name__ == '__main__':
-	main()
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')

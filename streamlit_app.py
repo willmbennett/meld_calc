a8af81f6f5c43b45f2338dc56ac72f1826ab36c4
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-07-03 12:15:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,26 @@
+# Imports
 import streamlit as st
 import pandas as pd
-import numpy as np
+import pickle
 
-st.title('Uber pickups in NYC')
+df_clean = pd.read_csv('mimic_iv_cleaned.csv')
+X = df_clean.drop(['target', 'meld'], axis=1)
+y = df_clean['target']
+
+# Now - predicting!
+if st.button(label="Click to Predict"):
+
+    # Load the model
+    loaded_model = pickle.load(open('final_model.pkl', 'rb'))
+    # Make predictions (and get out pred probabilities)
+    pred = loaded_model.predict(X[:1])[0]
+    proba = loaded_model.predict_proba(X[:1])[:,1][0]
+    
+    # Sharing the predictions
+    if pred == 0:
+        st.write("### The person is predicted to survive 90 days")
+        st.write(f"Predicted probability of dying: {proba*100:.2f} %")
+
+    elif pred == 1:
+        st.write("### The person is NOT predicted to survive 90 days!")
+        st.write(f"Predicted probability of dying: {proba*100:.2f} %")

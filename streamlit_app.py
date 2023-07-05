882ee4b314f5c17b561c909fe5d3a624b18d5bc7
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-07-05 13:50:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ df_clean = pd.read_csv('data/mimic_iv_cleaned.csv')
 # Load the model
 loaded_model = pickle.load(open('models/XGB_SFM.pkl', 'rb'))
 
-print(loaded_model)
-
 # Opening intro text
 st.write("# Calculate Outcome for Cirrhosis Patient")
 
@@ -26,22 +24,9 @@ X = selected_patient_data.drop(['target'], axis=1)
 y = selected_patient_data['target']
 st.write(f'patient index {X.index}')
 
-st.write("## Predict Patient Outcome:")
 
-# Make predictions (and get out pred probabilities)
-pred = loaded_model.predict(X)[0]
-proba = loaded_model.predict_proba(X)[:,1][0]
-
-# Sharing the predictions
-if pred == 0:
-    st.write("### The person is predicted to survive 90 days")
-    st.write(f"Predicted probability of dying: {proba*100:.2f} %")
-
-elif pred == 1:
-    st.write("### The person is NOT predicted to survive 90 days!")
-    st.write(f"Predicted probability of dying: {proba*100:.2f} %")
-
-    num_cols = ['inr_min',
+# Sidebar
+num_cols = ['inr_min',
             'inr_max',
             'bun_min',
             'aniongap_min',
@@ -56,13 +41,39 @@ elif pred == 1:
 cat_cols = ['gender', 'race']
 
 with st.sidebar:
-    # INR 
-    with st.expander("International Normalised Ratio (INR)"):
+    # INR Min
+    with st.expander("International Normalised Ratio (INR)" (Min)):
         st.write('A normal INR is 1.0. Each increase of 0.1 means the blood is slightly thinner (it takes longer to clot). INR is related to the prothrombin time (PT).')
-        st.write('[Veteran Affairs](https://www.hepatitis.va.gov/hcv/patient/diagnosis/labtests-INR.asp#:~:text=A%20normal%20INR%20is%201.0,the%20prothrombin%20time%20(PT).)')
+        st.write('- [Veteran Affairs](https://www.hepatitis.va.gov/hcv/patient/diagnosis/labtests-INR.asp#:~:text=A%20normal%20INR%20is%201.0,the%20prothrombin%20time%20(PT).)')
         
-        inr_label = 'Min:'
-        inr_default_value = X['inr_min'].iloc[0]
-        inr_min = st.number_input(inr_label, value=inr_default_value, step=0.1)
-        st.write('New INR Min:', inr_min)
+        inr_min_label = 'INR Min:'
+        inr_min_default_value = X['inr_min'].iloc[0]
+        inr_min = st.number_input(inr_min_label, value=inr_min_default_value, step=0.1)
+        st.write('New INR Min:', round(inr_min,1))
         X['inr_min'] = inr_min
+
+    # INR Max
+    with st.expander("International Normalised Ratio (INR) (Max)"):
+        st.write('A normal INR is 1.0. Each increase of 0.1 means the blood is slightly thinner (it takes longer to clot). INR is related to the prothrombin time (PT).')
+        st.write('- [Veteran Affairs](https://www.hepatitis.va.gov/hcv/patient/diagnosis/labtests-INR.asp#:~:text=A%20normal%20INR%20is%201.0,the%20prothrombin%20time%20(PT).)')
+        
+        inr_max_label = 'INR Max:'
+        inr_max_default_value = X['inr_max'].iloc[0]
+        inr_min = st.number_input(inr_max_label, value=inr_max_default_value, step=0.1)
+        st.write('New INR Max:', round(inr_max,1))
+        X['inr_max'] = inr_max
+
+st.write("## Predict Patient Outcome:")
+
+# Make predictions (and get out pred probabilities)
+pred = loaded_model.predict(X)[0]
+proba = loaded_model.predict_proba(X)[:,1][0]
+
+# Sharing the predictions
+if pred == 0:
+    st.write("### The person is predicted to survive 90 days")
+    st.write(f"Predicted probability of dying: {proba*100:.2f} %")
+
+elif pred == 1:
+    st.write("### The person is NOT predicted to survive 90 days!")
+    st.write(f"Predicted probability of dying: {proba*100:.2f} %")

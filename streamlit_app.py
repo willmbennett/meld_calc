4c94a107ad95d0924624a29e70489fde2af36114
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-07-05 13:45:01 -0700

    Sidebar for streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,34 +26,8 @@ X = selected_patient_data.drop(['target'], axis=1)
 y = selected_patient_data['target']
 st.write(f'patient index {X.index}')
 
-st.write(f"Total Number of features:", len(X.columns))
+st.write("## Predict Patient Outcome:")
 
-num_cols = {'inr_min',
-            'pt_min',
-            'ptt_min',
-            'bun_min',
-            'bilirubin_total_min',
-            'bilirubin_total_max',
-            'bun_max',
-            'inr_max'}
-
-cat_cols = ['gender', 'race']
-
-# INR 
-st.write(f"### International Normalised Ratio (INR)")
-st.write('A normal INR is 1.0. Each increase of 0.1 means the blood is slightly thinner (it takes longer to clot). INR is related to the prothrombin time (PT).')
-st.write('[Veteran Affairs](https://www.hepatitis.va.gov/hcv/patient/diagnosis/labtests-INR.asp#:~:text=A%20normal%20INR%20is%201.0,the%20prothrombin%20time%20(PT).)')
-
-inr_label = 'Min:'
-inr_default_value = X['inr_min'].iloc[0]
-inr_min = st.number_input(inr_label, value=inr_default_value, step=0.1)
-st.write('New INR Min:', inr_min)
-X['inr_min'] = inr_min
-
-st.write(f"## Predict Patient Outcome:")
-
-# Now - predicting!
-# if st.button(label="Click to Predict"):
 # Make predictions (and get out pred probabilities)
 pred = loaded_model.predict(X)[0]
 proba = loaded_model.predict_proba(X)[:,1][0]
@@ -66,3 +40,29 @@ if pred == 0:
 elif pred == 1:
     st.write("### The person is NOT predicted to survive 90 days!")
     st.write(f"Predicted probability of dying: {proba*100:.2f} %")
+
+    num_cols = ['inr_min',
+            'inr_max',
+            'bun_min',
+            'aniongap_min',
+            'bilirubin_total_min',
+            'Kmeans Cluster',
+            'alp_min',
+            'age',
+            'ptt_min',
+            'pt_min',
+            'pt_max']
+
+cat_cols = ['gender', 'race']
+
+with st.sidebar:
+    # INR 
+    with st.expander("International Normalised Ratio (INR)"):
+        st.write('A normal INR is 1.0. Each increase of 0.1 means the blood is slightly thinner (it takes longer to clot). INR is related to the prothrombin time (PT).')
+        st.write('[Veteran Affairs](https://www.hepatitis.va.gov/hcv/patient/diagnosis/labtests-INR.asp#:~:text=A%20normal%20INR%20is%201.0,the%20prothrombin%20time%20(PT).)')
+        
+        inr_label = 'Min:'
+        inr_default_value = X['inr_min'].iloc[0]
+        inr_min = st.number_input(inr_label, value=inr_default_value, step=0.1)
+        st.write('New INR Min:', inr_min)
+        X['inr_min'] = inr_min
